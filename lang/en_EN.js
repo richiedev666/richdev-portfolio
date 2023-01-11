7dd600371e1c95f862be20bcac752938c05008a9
selim <selimdev00@gmail.com>
2023-01-11 13:00:18 +0500

    [translation]: some english words translated

--- lang/en_EN.js
+++ lang/en_EN.js
@@ -1,3 +1,13 @@
 module.exports = {
   currentLanguage: 'English',
+  home: 'Home',
+  aboutMe: 'About me',
+  skills: 'Skills',
+  works: 'Works',
+  experience: 'Experience',
+  contacts: 'Contacts',
+  viewPortfolio: 'View portfolio',
+  hireMe: 'Hire me',
+  myWorks: 'My works',
+  viewAllWorks: 'View all works',
 }
