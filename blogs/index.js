034a26468dc43afc5e8260669da4dbbcfd35cda0
selim <selimdev00@gmail.com>
2023-03-04 10:13:43 +0500

    [blogs]: configured blogs ecosystem

--- blogs/index.js
+++ blogs/index.js
@@ -5,6 +5,64 @@ export default {
       title: 'Мой первый пост',
       description: 'Это мой первый пост 🤷‍♂️',
       image: require('~/assets/blog/blog-1.gif'),
+      date: '03.04.2023',
+      tags: ['Перерождение'],
+      content: `
+
+<div class="block">
+        <h1 class="title-h1">Мой первый пост</h1>
+
+          <p class="text">Добро пожаловать на мой блог! Это мой первый пост, и я так рад начать.</p>
+
+              <div class="image-full-width">
+                  <img src="/blog/rebirth-chapter-1.webp" alt="Глава 1" >
+              </div>
+          </div>
+
+          <div class="block">
+              <h2 class="title-h2">Обо мне</h2>
+
+              <p class="text">Меня зовут Ричи, и я программист. Я обожаю программирование и изучение новых технологий. В свободное время я люблю читать, играть и заниматься дизайном.</p>
+          </div>
+
+          <div class="block">
+              <h2 class="title-h2">Мои интересы</h2>
+              <ul class="list">
+                <li class="text">Программирование</li>
+                <li class="text">Чтение</li>
+                <li class="text">Игры</li>
+                <li class="text">Дизайн</li>
+              </ul>
+          </div>
+
+          <div class="block">
+            <h2 class="title-h2">Мои цели</h2>
+
+            <ol class="list-numbered">
+              <li class="text">Изучить новый язык программирования</li>
+              <li class="text">Прочитать 10 книг в этом году</li>
+              <li class="text">Создать видеоигру</li>
+              <li class="text">Создать новую концепцию дизайна</li>
+            </ol>
+          </div>
+
+          <div class="block">
+          <h2 class="title-h2">Моя любимая цитата</h2>
+            <div class="blockquote">
+              <blockquote>
+                <p>Будьте изменением, которое вы хотите увидеть в мире.</p>
+              </blockquote>
+              <cite>Махатма Ганди</cite>
+            </div>
+          </div>
+
+
+          <div class="block"><h2 class="title-h2">Что ожидать от моего блога</h2>
+          <p class="text">На этом блоге я буду делиться своими мыслями на различные темы, связанные с технологиями, книгами, играми и дизайном. Я также буду делиться своими опытом и учебным процессом по ходу дела. Надеюсь, что мои записи будут интересными и информативными для вас!</p></div>
+
+          <div class="block"><h2 class="title-h2">Заключение</h2>
+          <p class="text">Спасибо за чтение моего первого поста! Ожидайте больше постов в будущем.</p></div>
+      `,
     },
   ],
 
@@ -14,6 +72,64 @@ export default {
       title: 'My first post',
       description: 'My first post 👌🤷‍♂️',
       image: require('~/assets/blog/blog-1.gif'),
+      date: '03.04.2023',
+      tags: ['Rebirth'],
+      content: `
+
+    <div class="block">
+        <h1 class="title-h1">My First Post</h1>
+
+        <p class="text">Welcome to my blog! This is my first post and I'm so excited to get started.</p>
+
+        <div class="image-full-width">
+            <img src="/blog/rebirth-chapter-1.webp" alt="Chapter 1" >
+        </div>
+    </div>
+
+    <div class="block">
+        <h2 class="title-h2">About Me</h2>
+
+        <p class="text">My name is Richie and I'm a software engineer. I love coding and learning new technologies. Outside of work, I enjoy reading, gaming, and designing.</p>
+    </div>
+
+    <div class="block">
+        <h2 class="title-h2">My Interests</h2>
+        <ul class="list">
+          <li class="text">Coding</li>
+          <li class="text">Reading</li>
+          <li class="text">Gaming</li>
+          <li class="text">Designing</li>
+        </ul>
+    </div>
+
+    <div class="block">
+      <h2 class="title-h2">My Goals</h2>
+
+      <ol class="list-numbered">
+        <li class="text">Learn a new programming language</li>
+        <li class="text">Read 10 books this year</li>
+        <li class="text">Create a video game</li>
+        <li class="text">Create new design concept</li>
+      </ol>
+    </div>
+
+    <div class="block">
+    <h2 class="title-h2">My Favorite Quote</h2>
+      <div class="blockquote">
+        <blockquote>
+          <p>Be the change you wish to see in the world.</p>
+        </blockquote>
+        <cite> Mahatma Gandhi</cite>
+      </div>
+    </div>
+
+
+    <div class="block"><h2 class="title-h2">What to Expect from My Blog</h2>
+    <p class="text">In this blog, I'll be sharing my thoughts on various topics related to technology, books, gaming, and desiging. I'll also be sharing my experiences and learnings along the way. I hope you'll find my posts interesting and informative!</p></div>
+
+    <div class="block"><h2 class="title-h2">Wrapping Up</h2>
+    <p class="text">Thanks for reading my first post! Stay tuned for more posts to come.</p></div>
+      `,
     },
   ],
 }
--- pages/blog/index.vue
+++ pages/blog/index.vue
@@ -1,24 +1,17 @@
 <template>
   <div class="blog__wrapper">
+    <div class="home-section__background"></div>
+
+    <div class="home-section__background__effect"></div>
+
     <div class="container">
       <div class="blog">
-        <div
+        <BlogCard
           v-for="(item, index) in blogItems"
           :key="`blog-item-${index}`"
-          class="blog__item"
-        >
-          <nuxt-link :to="`/blog/${index + 1}`">
-            <div class="blog__item__image">
-              <img :src="item.image" alt="" />
-            </div>
-          </nuxt-link>
-
-          <div class="blog__item__content">
-            <h2>{{ item.title }}</h2>
-
-            <p>{{ item.description }}</p>
-          </div>
-        </div>
+          :item="item"
+          :index="index"
+        />
       </div>
     </div>
   </div>
@@ -26,10 +19,11 @@
 
 <script>
 import Blogs from '@/blogs'
+import BlogCard from '~/components/Blog/BlogCard'
 
 export default {
   name: 'BlogPage',
-
+  components: { BlogCard },
   computed: {
     blogItems() {
       return Blogs[this.$i18n.locale] || []
@@ -39,50 +33,18 @@ export default {
 </script>
 
 <style lang="scss" scoped>
+@import '~/assets/scss/components/sections/home-section.scss';
+
 .blog {
   &__wrapper {
-    padding-top: var(--header-height);
     background-color: var(--primary-yellow-color);
   }
 
+  padding-top: calc(var(--header-height) + 1rem);
+  position: relative;
+  z-index: 1;
   display: grid;
   grid-template-columns: repeat(3, 1fr);
   gap: 30px;
-
-  &__item {
-    background-color: rgba(0, 0, 0, 0.5);
-    border-radius: 10px;
-    color: #fff;
-    padding: 10px;
-    display: flex;
-    flex-direction: column;
-    gap: 14px;
-
-    &__image {
-      width: 100%;
-      height: 300px;
-
-      img {
-        object-fit: cover;
-        object-position: center;
-        width: 100%;
-        height: 100%;
-      }
-    }
-
-    &__content {
-      display: flex;
-      flex-direction: column;
-      gap: 5px;
-
-      h2 {
-        font-size: clamp(1.125rem, 0.6563rem + 1.5vw, 1.5rem);
-      }
-
-      p {
-        font-size: clamp(0.75rem, 0.5938rem + 0.5vw, 0.875rem);
-      }
-    }
-  }
 }
 </style>
