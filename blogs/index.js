10e35751bcacbddf07ec55d35512d121210b7f3a
selim <selimdev00@gmail.com>
2023-03-06 14:06:34 +0500

    [blog]: added new post

--- blogs/index.js
+++ blogs/index.js
@@ -5,7 +5,7 @@ export default {
       title: 'Мой первый пост',
       description: 'Это мой первый пост 🤷‍♂️',
       image: '/blog/previews/blog-1.gif',
-      date: '03.04.2023',
+      date: '04.03.2023',
       tags: ['Перерождение'],
       content: `
 
@@ -64,6 +64,83 @@ export default {
           <p class="text">Спасибо за чтение моего первого поста! Ожидайте больше постов в будущем.</p></div>
       `,
     },
+    {
+      id: 2,
+      title: '10 советов, как сделать работу вовремя',
+      description: 'Прочтите, прежде чем браться за следующий проект',
+      image: 'https://media.tenor.com/Zkckhw-amYMAAAAM/rado-horse.gif',
+      date: '04.03.2023',
+      tags: ['Продуктивность'],
+      content: `
+        <div class="block">
+            <h1 class="title-h1">10 советов, как сделать работу вовремя</h1>
+
+            <div class="text">Как разработчик, соблюдение сроков проекта имеет решающее значение. Тем не менее, может быть сложно оставаться в курсе всего и выполнять работу вовремя. В этом посте мы обсудим 10 советов, которые помогут вам повысить производительность и обеспечить соблюдение сроков.</div>
+        </div>
+
+        <div class="block">
+            <ol class="list-numbered">
+              <li>
+                <h2 class="title-h2">Ставьте четкие цели и приоритеты</h2>
+                <p class="text">Перед началом любого проекта установите четкие цели и приоритеты. Знайте, чего вы хотите достичь, и расставьте приоритеты задач в зависимости от их важности.</p>
+              </li>
+
+              <li>
+                <h2 class="title-h2">Создать временную шкалу</h2>
+                <p class="text">Создание временной шкалы поможет вам визуализировать свой прогресс и отслеживать сроки. Разбейте свой проект на более мелкие, управляемые задачи и установите реалистичные сроки для каждой из них.</p>
+              </li>
+
+              <li>
+                <h2 class="title-h2">Используйте инструмент управления задачами</h2>
+                <p class="text">Инструменты управления задачами, такие как Trello, Asana или Jira, помогут вам оставаться организованным и более эффективно управлять своими задачами. Они позволяют создавать списки задач, назначать сроки и отслеживать прогресс.</p>
+              </li>
+
+              <li>
+                <h2 class="title-h2">Сведите к минимуму отвлекающие факторы</h2>
+                <p class="text">Отвлекающие факторы могут существенно повлиять на вашу продуктивность. Постарайтесь свести к минимуму отвлекающие факторы, выключив телефон, закрыв ненужные вкладки в браузере или найдя тихое рабочее место.</p>
+              </li>
+
+              <li>
+                <h2 class="title-h2">Делайте перерывы</h2>
+                <p class="text">Перерывы необходимы для продуктивной работы. Это может помочь вам перезарядиться и оставаться сосредоточенным. Делайте короткие перерывы каждый час или около того, чтобы избежать выгорания.</p>
+              </li>
+
+              <li>
+                <h2 class="title-h2">Научись говорить нет</h2>
+                <p class="text">Ответ «да» на каждый запрос может быстро ошеломить вас и повлиять на вашу производительность. Научитесь вежливо говорить «нет» и соответствующим образом расставляйте приоритеты в своих задачах.</p>
+              </li>
+
+              <li>
+                <h2 class="title-h2">Используйте технику помидора</h2>
+                <p class="text">Техника Pomodoro — это техника тайм-менеджмента, которая включает в себя работу с 25-минутными интервалами с короткими перерывами между ними. Это может помочь вам оставаться сосредоточенным и более эффективно управлять своим временем.</p>
+              </li>
+
+              <li>
+                <h2 class="title-h2">Общайтесь со своей командой</h2>
+                <p class="text">Коммуникация является ключом к успешному управлению проектом. Держите вашу команду в курсе вашего прогресса, обращайтесь за помощью, когда это необходимо, и сообщайте о любых проблемах, которые могут возникнуть.</p>
+              </li>
+
+              <li>
+                <h2 class="title-h2">Не откладывай</h2>
+                <p class="text">Прокрастинация может быть вашим злейшим врагом. Очень важно начать работу над своими задачами как можно раньше, чтобы избежать стресса в последнюю минуту и убедиться, что у вас достаточно времени, чтобы выполнить все.</p>
+              </li>
+
+              <li>
+                <h2 class="title-h2">Просмотрите и отрегулируйте</h2>
+                <p class="text">После завершения проекта найдите время, чтобы просмотреть свой прогресс и при необходимости скорректировать свой подход. Определите, что сработало хорошо, а что нет, и используйте эту информацию для повышения производительности в будущих проектах.</p>
+              </li>
+            </ol>
+        </div>
+
+        <div class="block">
+            <h2 class="title-h2">Заключение:</h2>
+
+            <p class="text">Следуя этим советам, вы значительно повысите свою производительность и сможете выполнять работу вовремя. Помните, что поддержание организованности, расстановка приоритетов задач и эффективное управление временем являются ключом к успеху в любом проекте.</p>
+        </div>
+
+
+      `,
+    },
   ],
 
   en: [
@@ -72,7 +149,7 @@ export default {
       title: 'My first post',
       description: 'My first post 👌🤷‍♂️',
       image: '/blog/previews/blog-1.gif',
-      date: '03.04.2023',
+      date: '04.03.2023',
       tags: ['Rebirth'],
       content: `
 
@@ -129,6 +206,82 @@ export default {
 
     <div class="block"><h2 class="title-h2">Wrapping Up</h2>
     <p class="text">Thanks for reading my first post! Stay tuned for more posts to come.</p></div>
+      `,
+    },
+    {
+      id: 2,
+      title: '10 tips how to get job done on time',
+      description: 'Read Before Taking on the Next Project',
+      image: 'https://media.tenor.com/Zkckhw-amYMAAAAM/rado-horse.gif',
+      date: '06.03.2023',
+      tags: ['Productivity'],
+      content: `
+
+        <div class="block">
+            <h1 class="title-h1">10 tips how to get job done on time</h1>
+
+            <div class="text">As a developer, meeting project deadlines is crucial. However, it can be challenging to stay on top of everything and get the job done on time. In this post, we'll discuss 10 tips to help you improve your productivity and ensure you meet your deadlines.</div>
+        </div>
+
+        <div class="block">
+            <ol class="list-numbered">
+              <li>
+                <h2 class="title-h2">Set clear goals and priorities</h2>
+                <p class="text">Before starting any project, set clear goals and priorities. Know what you want to accomplish and prioritize your tasks based on their importance</p>
+              </li>
+
+              <li>
+                <h2 class="title-h2">Create a timeline</h2>
+                <p class="text">Creating a timeline helps you visualize your progress and keep track of your deadlines. Break your project down into smaller, manageable tasks and set realistic deadlines for each one.</p>
+              </li>
+
+              <li>
+                <h2 class="title-h2">Use a task management tool</h2>
+                <p class="text">Task management tools like Trello, Asana, or Jira can help you stay organized and manage your tasks more efficiently. They allow you to create task lists, assign deadlines, and track your progress.</p>
+              </li>
+
+              <li>
+                <h2 class="title-h2">Minimize distractions</h2>
+                <p class="text">Distractions can significantly impact your productivity. Try to minimize distractions by turning off your phone, closing unnecessary tabs on your browser, or finding a quiet workspace.</p>
+              </li>
+
+              <li>
+                <h2 class="title-h2">Take breaks</h2>
+                <p class="text">Taking breaks is essential for staying productive. It can help you recharge and stay focused. Take short breaks every hour or so to avoid burnout.</p>
+              </li>
+
+              <li>
+                <h2 class="title-h2">Learn to say no</h2>
+                <p class="text">Saying yes to every request can quickly overwhelm you and impact your productivity. Learn to say no politely and prioritize your tasks accordingly.</p>
+              </li>
+
+              <li>
+                <h2 class="title-h2">Use the Pomodoro technique</h2>
+                <p class="text">The Pomodoro technique is a time-management technique that involves working in 25-minute intervals with short breaks in between. It can help you stay focused and manage your time more efficiently.</p>
+              </li>
+
+              <li>
+                <h2 class="title-h2">Communicate with your team</h2>
+                <p class="text">Communication is key to successful project management. Keep your team informed of your progress, ask for help when needed, and communicate any issues that may arise.</p>
+              </li>
+
+              <li>
+                <h2 class="title-h2">Don't procrastinate</h2>
+                <p class="text">Procrastination can be your worst enemy. It's essential to start working on your tasks as soon as possible to avoid last-minute stress and ensure you have enough time to complete everything.</p>
+              </li>
+
+              <li>
+                <h2 class="title-h2">Review and adjust</h2>
+                <p class="text">After completing your project, take the time to review your progress and adjust your approach if necessary. Identify what worked well and what didn't, and use this information to improve your productivity in future projects.</p>
+              </li>
+            </ol>
+        </div>
+
+        <div class="block">
+            <h2 class="title-h2">Conclusion:</h2>
+
+            <p class="text">By following these tips, you can significantly improve your productivity and ensure you get the job done on time. Remember, staying organized, prioritizing your tasks, and managing your time effectively are key to success in any project.</p>
+        </div>
       `,
     },
   ],
